ab3eaed335ce24cd915dabb47f74fbeb05e62606
yusiukui <67853488+yusiukui@users.noreply.github.com>
2020-10-21 15:29:37 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+function genrand (num: number, num2: number) {
+    if (0 >= 0) {
+    	
+    } else {
+    	
+    }
+}
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(control.millis() % (input.acceleration(Dimension.X) + input.lightLevel()))
+})
+basic.forever(function () {
+	
+})
